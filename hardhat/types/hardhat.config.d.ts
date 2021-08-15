bb34774e1c879ef7302003f046b427620d19abba
zen <zen2see@gmail.com>
2021-08-15 17:19:49 -0400

    SETUP AND CONFIG Done

---
+++ hardhat/types/hardhat.config.d.ts
@@ -0,0 +1,11 @@
+import '@typechain/hardhat';
+import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-etherscan';
+import 'hardhat-contract-sizer';
+import 'solidity-coverage';
+import 'tsconfig-paths/register';
+import { HardhatUserConfig } from 'hardhat/types';
+declare const config: HardhatUserConfig;
+export default config;
+//# sourceMappingURL=hardhat.config.d.ts.map
